825e83e1809caa8fada1695ec21b7d88cbd75840
Suresh Melvin Sigera <sureshsigera@gmail.com>
2024-01-03 14:24:07 -0500

    updated ListItem with handleDeleteTodo

--- react-to-do-frontend-starter/src/ListItem.js
+++ react-to-do-frontend-starter/src/ListItem.js
@@ -1,9 +1,10 @@
-const ListItem = ({title, done, id}) => {
+const ListItem = ({title, done, id, handleDeleteTodo}) => {
     return (
         <>
             <li>
                 {title}
                 <input type="checkbox" defaultChecked={done}/>
+                <button onClick={(e) => handleDeleteTodo(e, id)}>X</button>
             </li>
         </>
     );
