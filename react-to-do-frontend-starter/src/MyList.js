697b79ded6c52d9a21369540cdffb927e5df9d7d
Suresh Melvin Sigera <sureshsigera@gmail.com>
2024-03-06 21:05:23 -0500

    code along lesson ready

--- react-to-do-frontend-starter/src/MyList.js
+++ react-to-do-frontend-starter/src/MyList.js
@@ -1,141 +1,69 @@
 import ListItem from "./ListItem";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 function MyList() {
     const [toDoItemArray, setToDoItemArray] = useState([]);
     const [newItem, setNewItem] = useState("");
 
+    // Todo item 1: Render todo items
     const todoItems = toDoItemArray.map((item) => (
-        <ListItem {...item}
-                  key={item.id}
-                  handleDeleteTodo={deleteTodo}
-                  handleUpdateTodo={updateTodo}
+        <ListItem
+            {...item}
+            key={item.id}
+            handleDeleteTodo={deleteTodo}
+            handleUpdateTodo={updateTodo}
         />
     ));
 
-    // We declare a named function called getTodos()
-    // because an fetch and .json are asynchronous.
-    // You can put all of this inside useEffect if you want
+    // Todo item 2: Fetch todos from API
     async function getTodos() {
-        const response = await fetch("http://127.0.0.1:3001/api/todos");
-        const todos = await response.json();
-        console.log("Todos from database", todos);
-
-        setToDoItemArray(todos);
+       
     }
 
-    // Inside the [], tell useEffect to update
-    // only when a particular value changes
-    // React will do an infinite update if you do not
-    // supply an empty dependencies array
+    // Todo item 3: Use effect to fetch todos on component mount
     useEffect(() => {
-        getTodos();
+        console.log("calling useEffect")
     }, []);
 
-    return (
-        <div>
-            <h1>Things I should stop procrastinating:</h1>
-            <ul>{todoItems}</ul>
-            {newItem}
-            <form>
+    // Todo item 4: Form to add new todo
+    function renderForm() {
+        return (
+            <form onSubmit={createTodo}>
                 <input
                     type="text"
                     placeholder="Type an item here"
                     onChange={(e) => setNewItem(e.target.value)}
                     value={newItem}
                 />
-                <button onClick={createTodo}>
-                    Add it!
-                </button>
+                <button type="submit">Add it!</button>
             </form>
-            <button
-                onClick={() => {
-                    setToDoItemArray([]);
-                }}
-            >
-                Finished the list!
-            </button>
-        </div>
-    );
+        );
+    }
 
+    // Todo item 5: Create a new todo item
     async function createTodo(e) {
         e.preventDefault();
-
-        let newItemForDatabase = {
-            title: newItem,
-            done: false,
-        };
-
-        const options = {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newItemForDatabase),
-        };
-        try {
-            const response = await fetch(`http://127.0.0.1:3001/api/todos`, options);
-            const data = await response.json();
-            console.log("newTodo With ID", data);
-            setToDoItemArray([...toDoItemArray, data]);
-            setNewItem("");
-        } catch (error) {
-            console.log(error);
-        }
     }
 
+    // Todo item 6: Delete a todo item
     async function deleteTodo(e, id) {
         e.preventDefault();
-
-        const options = {
-            method: "DELETE",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-        };
-        try {
-            const response = await fetch(
-                `http://127.0.0.1:3001/api/todos/${id}`,
-                options
-            );
-            let newToDoItemArray = [...toDoItemArray];
-            let itemToDelete = newToDoItemArray.findIndex((item) => item.id === id);
-            newToDoItemArray.splice(itemToDelete, 1);
-            setToDoItemArray(newToDoItemArray);
-        } catch (error) {
-            console.log(error);
-        }
     }
 
+    // Todo item 7: Update a todo item
     async function updateTodo(e, id) {
         e.preventDefault();
-
-        let indexOfItem = toDoItemArray.findIndex((item) => item.id === id);
-        let itemToUpdate = {...toDoItemArray[indexOfItem]};
-        itemToUpdate.done = !itemToUpdate.done;
-
-        const options = {
-            method: "PUT",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(itemToUpdate),
-        };
-        try {
-            const response = await fetch(
-                `http://127.0.0.1:3001/api/todos/${id}`,
-                options
-            );
-            const newToDoItemArray = [...toDoItemArray];
-            newToDoItemArray[indexOfItem] = itemToUpdate;
-            setToDoItemArray(newToDoItemArray);
-        } catch (error) {
-            console.log(error);
-        }
     }
+
+    // Rendering the component UI
+    return (
+        <div>
+            <h1>Things I should stop procrastinating:</h1>
+            <ul>{todoItems}</ul>
+            {renderForm()}
+            <button onClick={() => setToDoItemArray([])}>Finished the list!</button>
+        </div>
+    );
 }
 
 export default MyList;
